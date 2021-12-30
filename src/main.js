bcd22cc058413b8e6ae431542694a94812e391c9
yuanbaoyaoyao <583515921@qq.com>
2021-12-30 11:17:04 +0800

    dashboard-basic

--- src/main.js
+++ src/main.js
@@ -6,8 +6,27 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import {
+    library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    fas
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    fab
+} from '@fortawesome/free-brands-svg-icons';
+import {
+    FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+import {
+    dom
+} from '@fortawesome/fontawesome-svg-core'
 
-const app = createApp(App)
+
+library.add(fas, fab)
+dom.watch()
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
     .use(ElementPlus, {
         locale: zhCn,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,52 @@
 <template>
     <div class="dashboard">
         <div class="dashboard-header">
-            <div class="dashboard-logo">耗材管理系统</div>
-            <div class="dashboard-search">search</div>
+            <div class="dashboard-logo">
+                <a href="#">
+                    <img src="../assets/sysLogo.svg" style="display:block;width:110px;height:auto" />
+                </a>
+            </div>
+            <div>
+                <div class="dashboard-search-top">
+                    <div>
+                        <el-input
+                            v-model="input"
+                            placeholder="Please input"
+                            :prefix-icon="Search"
+                            clearable
+                        />
+                    </div>
+                    <div>
+                        <el-button type="primary">Search</el-button>
+                    </div>
+                </div>
+                <div class="dashboard-search-down">
+                    <a
+                        :href="item.linkname"
+                        target="_blank"
+                        v-for="(item,index) in arraySearch"
+                        :key="index"
+                    >{{ item.name }}</a>
+                </div>
+            </div>
             <div class="dashboard-blank"></div>
         </div>
         <div class="dashboard-content">
             <div class="dashboard-content-left">
-                <div>耗材选择</div>
+                <h4>耗材选择</h4>
                 <div class="dashboard-consumables-list">
-                    <ul v-for="(item,index) in listData" :key="index">
+                    <ul v-for="(item,index) in listData" :key="index" class="list-ul">
                         <li class="dashboard-consumables-list-detail">
-                            <a :href="item.linkname" target="_blank">{{ item.name }}</a>
-                            <span>/</span>
-                            <a :href="item.linkevent" target="_blank">{{ item.event }}</a>
+                            <div class="list-detail-icon">
+                                <i :class="item.icon"></i>
+                            </div>
+                            <div class="list-detail-text">
+                                <a :href="item.linkname" target="_blank">{{ item.name }}</a>
+                                <span>/</span>
+                                <a :href="item.linkevent" target="_blank">{{ item.event }}</a>
+                                <span>/</span>
+                                <a :href="item.linkevent" target="_blank">{{ item.event }}</a>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -26,36 +59,131 @@
                 </el-carousel>
             </div>
             <div class="dashboard-content-right">
-                <div class="dashboard-user">avatar</div>
-                <div class="dashboard-user-history">history</div>
+                <div class="dashboard-user-avatar">
+                    <el-avatar
+                        :size="80"
+                        src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                    ></el-avatar>
+                    <h3>欢迎</h3>
+                </div>
+                <div class="dashboard-user-notice">
+                    <span class="notice-title">
+                        <b>公告</b>
+                    </span>
+                    <span>新鲜事儿都在这里～</span>
+                </div>
+                <div class="dashboard-user-button">
+                    <div class="button-info">
+                        <el-icon>
+                            <star />
+                        </el-icon>
+                        <el-button type="text">
+                            <span class="button-text">我的收藏</span>
+                        </el-button>
+                    </div>
+                    <div class="button-info">
+                        <el-icon>
+                            <document-checked />
+                        </el-icon>
+                        <el-button type="text">
+                            <span class="button-text">我的订单</span>
+                        </el-button>
+                    </div>
+                    <div class="button-info">
+                        <el-icon>
+                            <warning />
+                        </el-icon>
+                        <el-button type="text">
+                            <span class="button-text">我的反馈</span>
+                        </el-button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script setup>
+import { Star, DocumentChecked, Warning, Search } from '@element-plus/icons-vue';
+import { ref } from 'vue';
+const input = ref('')
+const arraySearch = [
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+    {
+        name: "88a",
+        linkname: "http://www.baidu.com",
+    },
+]
 const listData = [
     {
-        name: "qwe",
+        icon: "fas fa-cubes",
+        name: "88a",
         linkname: "http://www.baidu.com",
-        event: "need a card",
+        event: "need a ",
         linkevent: "www.baidu.com"
     },
     {
-        name: "qwe",
+        icon: "fas fa-cubes",
+        name: "88a",
         linkname: "http://www.baidu.com",
-        event: "need a card",
+        event: "need a ",
         linkevent: "www.baidu.com"
     },
     {
-        name: "qwe",
+        icon: "fas fa-cubes",
+        name: "88a",
         linkname: "http://www.baidu.com",
-        event: "need a card",
+        event: "need a ",
         linkevent: "www.baidu.com"
     },
     {
-        name: "qwe",
+        icon: "fas fa-cubes",
+        name: "88a",
         linkname: "http://www.baidu.com",
-        event: "need a card",
+        event: "need a ",
+        linkevent: "www.baidu.com"
+    },
+    {
+        icon: "fas fa-cubes",
+        name: "88a",
+        linkname: "http://www.baidu.com",
+        event: "need a ",
+        linkevent: "www.baidu.com"
+    },
+    {
+        icon: "fas fa-cubes",
+        name: "88a",
+        linkname: "http://www.baidu.com",
+        event: "need a ",
+        linkevent: "www.baidu.com"
+    },
+    {
+        icon: "fas fa-cubes",
+        name: "88a",
+        linkname: "http://www.baidu.com",
+        event: "need a ",
         linkevent: "www.baidu.com"
     },
 ]
@@ -63,23 +191,110 @@ const listData = [
 <style>
 ul {
     list-style: none;
+    padding: 0px;
+}
+h3,
+h4 {
+    text-align: center;
+    margin: 0px;
+}
+img {
+    height: fit-content;
+    width: fit-content;
 }
 .dashboard {
     display: flex;
     flex-direction: column;
 }
-.dashboard-header,
-.dashboard-content {
+.dashboard-header {
+    display: grid;
+    grid-template-columns: 20% 40% auto;
+    justify-items: center;
+    margin: 25px;
+}
+.dashboard-search-top {
+    display: grid;
+    grid-template-columns: 70% auto;
+    justify-content: center;
+    margin-top: 10px;
+}
+.dashboard-search-down {
     display: flex;
 }
-.dashboard-header,
-.dashboard-content {
-    justify-content: space-between;
+.dashboard-search-down > a {
+    margin-left: 5px;
 }
 .dashboard-content {
+    display: grid;
+    grid-template-columns: 20% 44% auto;
     background-color: white;
     border-radius: 15px;
 }
+.dashboard-content-left,
+.dashboard-content-right,
+.dashboard-content-center {
+    margin: 20px;
+}
 .dashboard-consumables-list-detail {
+    display: grid;
+    grid-template-columns: auto auto;
+}
+.list-detail-icon {
+    text-align: center;
+}
+.list-detail-text {
+    display: flex;
+    justify-content: space-around;
+}
+.dashboard-content-right {
+    display: grid;
+    grid-template-rows: auto auto;
+    background-color: whitesmoke;
+}
+.dashboard-user-avatar {
+    margin: auto;
+}
+.dashboard-user-notice,
+.dashboard-user-button {
+    margin: 5px;
+}
+.dashboard-user-button {
+    display: grid;
+    grid-template-columns: auto auto auto;
+}
+.dashboard-logo-img {
+    font-size: x-large;
+}
+.button-info {
+    margin: auto;
+    display: grid;
+    grid-template-rows: auto auto;
+    justify-items: center;
+    align-items: center;
+    font-size: x-large;
+}
+.button-text {
+    font-size: large;
+}
+.notice-title {
+    margin-right: 10px;
+    font-family: tahoma, arial, "Hiragino Sans GB", 宋体, sans-serif;
+    font-size: large;
+}
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+    text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
 }
 </style>
